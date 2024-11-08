ea1c666143043615d9f31c7bd7248ee745df89d0
dfomins <danielsfomins92@gmail.com>
2024-11-08 10:54:40 +0200

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,50 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
-    <footer className="w-full flex justify-center py-10 bg-system-green text-white">
-      <div className="max-w-[1200px] flex">
-        <div className="w-auto">
+    <footer className="flex justify-center py-10 bg-system-green text-white">
+      <div className="w-11/12 xl:w-[1200px] lg:w-[950px] flex justify-between flex-wrap">
+        <div className="w-full md:w-6/12 mb-3">
           <h3 className="mb-3 font-semibold">Par mums</h3>
           <p>Intek - moderns portāls, lai jūs varētu veidot darba atskaites savā uzņēmumā. Viegla atskaišu sistēma ļaus atzīmēt stundu skaitu katram darbiniekam pāris minūšu garumā.</p>
         </div>
-        <div className="w-auto">
+        <div className="max-md:w-6/12 mb-3">
+          <h3 class="text-[20px] font-semibold">Saites</h3>
+          <ul>
+            <li>
+              <Link to="/">Profils</Link>
+            </li>
+            <li>
+              <Link to="/piezimes">Piezīmes</Link>
+            </li>
+            <li>
+              <Link to="/pazinojumi">Paziņojumi</Link>
+            </li>
+            <li>
+              <Link to="/darbs">Darbs</Link>
+            </li>
+            <li>
+              <Link to="/atskaite">Atskaite</Link>
+            </li>
+            <li>
+              <Link to="/darba_objekti">Objekti</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="mb-3">
           <h3 className="mb-3 font-semibold">Kontakti</h3>
-          <p>+371 12345678</p>
-          <p>info@intek.lv</p>
-          <p>Jelgava, Lielā iela 2</p>
+          <div className="flex gap-2 items-center">
+            <i class="fa-solid fa-phone" />
+            <p>+371 12345678</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <i class="fa-solid fa-envelope" />
+            <p>info@intek.lv</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <i class="fa-solid fa-location-dot" />
+            <p>Jelgava, Lielā iela 2</p>
+          </div>
         </div>
       </div>
     </footer>
