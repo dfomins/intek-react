0440955acb72cf7e8b5214a471ad180d510c8ec9
dfomins <danielsfomins92@gmail.com>
2024-11-11 19:24:16 +0200

    updated data and allusers page responive

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -9,38 +9,43 @@ function AllUsers() {
                     <button className="px-2 bg-system-blue hover:bg-system-green duration-100 rounded-sm text-white">Izveidot jaunu lietotāju</button>
                     <input type="text"  className="system-input" placeholder="Meklēt..."/>
                 </div>
-                <table className="w-full shadow-sm">
-                    <thead className="bg-system-blue text-white">
-                        <tr className="border border-black">
-                            <th className="p-2 text-start">Nr.</th>
-                            <th className="p-2 text-start">Vārds</th>
-                            <th className="p-2 text-start">Uzvārds</th>
-                            <th className="p-2 text-start">E-pasts</th>
-                            <th className="p-2 text-start">Objekti</th>
-                            <th className="p-2 text-start">Loma</th>
-                            <th className="p-2">Iestatījumi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map(user => (
-                            <tr key={user.id} className={`${user.id % 2 == 0 ? "bg-[#F3F3F3]" : "bg-white"}`}>
-                                <td className="p-2 text-start">{user.id}</td>
-                                <td className="p-2 text-start">{user.name}</td>
-                                <td className="p-2 text-start">{user.surname}</td>
-                                <td className="p-2 text-start">{user.email}</td>
-                                <td className="p-2 text-start">{user.objects}</td>
-                                <td className="p-2 text-start">{user.role}</td>
-                                <td className="p-2 text-center">
-                                    <i className="fa-solid fa-pen-to-square mr-3" />
-                                    <i className="fa-solid fa-trash"></i>
-                                </td>
+                <div className="h-[500px] overflow-auto">
+                    <table className="table-auto w-full shadow-sm">
+                        <thead className="text-white">
+                            <tr>
+                                <th className="p-3 text-start sticky top-0 left-0 z-10 bg-system-blue">Nr.</th>
+                                <th className="p-3 text-start sticky top-0 bg-system-blue">Vārds</th>
+                                <th className="p-3 text-start sticky top-0 bg-system-blue">Uzvārds</th>
+                                <th className="p-3 text-start sticky top-0 bg-system-blue">E-pasts</th>
+                                <th className="p-3 text-start sticky top-0 bg-system-blue">Objekti</th>
+                                <th className="p-3 text-start sticky top-0 bg-system-blue">Loma</th>
+                                <th className="min-w-[75px] p-3 sticky top-0 right-0 z-10 bg-system-blue"><i className="fa-solid fa-gear" /></th>
                             </tr>
-                        ))}
-                        <tr className="text-white">
-                            <td className="p-2 bg-system-blue text-start" colSpan={7}>Kopā: {users.length}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {users.map(user => (
+                                <tr key={user.id} className={`${user.id % 2 == 0 ? "bg-[#F3F3F3]" : "bg-white"}`}>
+                                    <td className={`p-3 text-start sticky left-0 ${user.id % 2 == 0 ? "bg-[#F3F3F3]" : "bg-white"}`}>{user.id}</td>
+                                    <td className="p-3 text-start">{user.name}</td>
+                                    <td className="p-3 text-start">{user.surname}</td>
+                                    <td className="p-3 text-start">{user.email}</td>
+                                    <td className="p-3 text-start">{user.objects}</td>
+                                    <td className="p-3 text-start">{user.role}</td>
+                                    <td className={`p-3 text-center sticky right-0 ${user.id % 2 == 0 ? "bg-[#F3F3F3]" : "bg-white"}`}>
+                                        <i className="fa-solid fa-pen-to-square mr-3 cursor-pointer" />
+                                        <i className="fa-solid fa-trash cursor-pointer"></i>
+                                    </td>
+                                </tr>
+                            ))}
+                            {/* <tr className="bg-white border-t-2 border-[#2b6777]">
+                                <td className="p-2 text-start" colSpan={7}>Kopā: {users.length}</td>
+                            </tr> */}
+                        </tbody>
+                    </table>  
+                </div>
+                <div className="p-3 bg-white border-t-2 border-[#2b6777]">
+                    Kopā: {users.length}
+                </div>
             </div>
         </div>
     )
--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -19,30 +19,86 @@ export const users = [{
     email: 'daniils@gmail.com',
     objects: '1',
     role: 'Brigadieris'
-}];
+}, {
+    id: 4,
+    name: 'Berthe',
+    surname: 'Hyde-Chambers',
+    email: 'bhydechambers2@cdc.gov',
+    objects: '2',
+    role: 'Brigadieris'
+}, {
+    id: 5,
+    name: 'Moshe',
+    surname: 'Nowill',
+    email: 'mnowill3@huffingtonpost.com',
+    objects: '1',
+    role: 'Brigadieris'
+}, {
+    id: 6,
+    name: 'Renee',
+    surname: 'Isted',
+    email: 'risted4@seesaa.net',
+    objects: '8',
+    role: 'Brigadieris'
+}, {
+    id: 7,
+    name: 'Janis',
+    surname: 'Isaacs',
+    email: 'jisaacs5@tmall.com',
+    objects: '2',
+    role: 'Brigadieris'
+}, {
+    id: 8,
+    name: 'Eadie',
+    surname: 'MacAndrew',
+    email: 'emacandrew6@creativecommons.org',
+    objects: '2',
+    role: 'Brigadieris'
+}, {
+    id: 9,
+    name: 'Dominica',
+    surname: 'Loughnan',
+    email: 'doloughnan7@blogs.com',
+    objects: '8',
+    role: 'Darbinieks'
+}, {
+    id: 10,
+    name: 'Eal',
+    surname: 'Roggero',
+    email: 'eroggero8@baidu.com',
+    objects: '2',
+    role: 'Darbinieks'
+}, {
+    id: 11,
+    name: 'Jeniffer',
+    surname: 'Doog',
+    email: 'jdoog9@hexun.com',
+    objects: '8',
+    role: 'Darbinieks'
+}]
 
 export const notes = [{
     id: 1,
     title: 'Pirmā piezīme',
     content: 'asdasdasd',
-    createdAt: new Date("2023-11-11")
+    createdAt: new Date('2023-11-11')
 }, {
     id: 2,
     title: 'Otrā piezīme',
     content: 'asdasdasd',
-    createdAt: new Date("2023-11-05")
+    createdAt: new Date('2023-11-05')
 }];
 
 export const notifications = [{
     id: 1,
     title: 'Pirmais paziņojums',
     content: 'asdasdasd',
-    createdAt: new Date("2023-11-05"),
-    createdBy: users[0].name + " " + users[0].surname
+    createdAt: new Date('2023-11-05'),
+    createdBy: users[0].name + ' ' + users[0].surname
 }, {
     id: 2,
     title: 'Otrais paziņojums',
     content: 'asdasdasd',
-    createdAt: new Date("2023-11-02"),
-    createdBy: users[0].name + " " + users[0].surname
+    createdAt: new Date('2023-11-02'),
+    createdBy: users[0].name + ' ' + users[0].surname
 }];
