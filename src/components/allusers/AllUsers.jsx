b7b5062b2998eedc76295057dbe27d891f9e5fb9
dfomins <danielsfomins92@gmail.com>
2026-01-18 14:02:02 +0200

    Fix

--- src/components/allusers/AllUsers.jsx
+++ src/components/allusers/AllUsers.jsx
@@ -169,9 +169,9 @@ function AllUsers() {
                                                                         <path
                                                                             fill="none"
                                                                             stroke="currentColor"
-                                                                            stroke-linecap="round"
-                                                                            stroke-linejoin="round"
-                                                                            stroke-width="1.5"
+                                                                            strokeLinecap="round"
+                                                                            strokeLinejoin="round"
+                                                                            strokeWidth="1.5"
                                                                             d="m5 19l7-7m0 0l7-7m-7 7L5 5m7 7l7 7"
                                                                         />
                                                                     </svg>
